cf65166cbed1ee17bc4a3c457d4cf0c28dfd0dfe
ZhichuCen <cenzhichu@outlook.com>
2022-08-14 19:36:29 +0800

    朗读功能（字符串索引好累人

--- main.py
+++ main.py
@@ -10,6 +10,10 @@ from playsound import playsound
 from sasr import sasr
 from tts import tts
 
+PUNC = {'。': '句号', '.': '点', '，': '逗号', ',': '逗号', '、': '顿号', '；': '分号', ';': '分号',
+        '：': '冒号', ':': '冒号', '？': '问号', '?': '问号', '！': '感叹号', '!': '感叹号',
+        '"': '引号', "'": '引号', '“': '引号', '”': '引号', '\n': '换行'}
+
 
 class ChunJi:
     def __init__(self):
@@ -64,6 +68,9 @@ class ChunJi:
             elif self.pending == "命名确认":
                 self.confirm_name()
 
+            elif self.pending == '朗读选择':
+                self.select_read_method()
+
         else:
             if self.mode == "Command":
                 self.command_method()
@@ -77,13 +84,16 @@ class ChunJi:
             else:
                 self.mode = "Insert"
                 self.speech("当前模式：输入")
+        #
+        # elif self.result_text in ['朗读上一句', '读上一句']:
+        #     self.read_content('上一句')
+        # elif self.result_text in ['朗读下一句', '读下一句']:
+        #     self.read_content('下一句')
+        # elif self.result_text in ['朗读全文', '全文朗读', '读全文']:
+        #     self.read_content('全文')
 
-        elif self.result_text in ['朗读上一句', '读上一句']:
-            self.read_content('上一句')
-        elif self.result_text in ['朗读下一句', '读下一句']:
-            self.read_content('下一句')
-        elif self.result_text in ['朗读全文', '全文朗读', '读全文']:
-            self.read_content('全文')
+        elif self.result_text in ['朗读', '读', ]:
+            self.read_aloud_method()
 
         elif self.result_text == "保存":
             self.save_file()
@@ -105,14 +115,32 @@ class ChunJi:
             self.saved = False
             print(self.text)
 
+    def read_aloud_method(self):
+        self.speech('请选择要朗读的内容')
+        self.pending = '朗读选择'
+
+    def select_read_method(self):
+
+        if self.result_text in ['全文', '所有', '全']:
+            self.read_content('全文')
+        elif self.result_text in ['上一句']:
+            self.read_content('上一句')
+        elif self.result_text in ['下一句']:
+            self.read_content('下一句')
+
+        else:
+            self.speech('请正确选择朗读内容')
+
     def read_content(self, content):
-        PUNC = {'。': '句号', '，': '逗号', '、': '顿号', '；': '分号', '：': '冒号', '？': '问号'}
-        if content == '上一句':
-            pass
-        elif content == '下一句':
-            pass
-        elif content == '全文':
+        self.pending = ''
+
+        if content == '全文':
             self.speech(self.text)
+
+        elif content == '上一句':
+            self.speech(self.selector('上一句'))
+        elif content == '下一句':
+            self.speech(self.selector('下一句'))
 
     def delete_method(self, content):
 
@@ -120,6 +148,28 @@ class ChunJi:
             pass
         elif content == '下一句':
             pass
+
+    def selector(self, area):
+
+        if area == '全文':
+            return self.text
+        elif area == '上一句':
+            left = self.text[0:self.cursor]
+            l = len(left)
+            for i in range(l):
+                t = left[-1 - i]
+                if t in PUNC.keys() and i != 0:
+                    return left[-i:]
+            return left
+
+        elif area == '下一句':
+            right = self.text[self.cursor:-1]
+            r = len(right)
+            for i in range(r):
+                t = right[i]
+                if t in PUNC.keys() and i != 0:
+                    return right[:i + 1]
+            return right
 
     def save_file(self):
         with open(self.name, 'w') as f:
@@ -152,7 +202,7 @@ class ChunJi:
 
     def new_name(self):
         self.unconfirmed_name = self.result_text + ".txt"
-        self.speech("将命名为：" + self.unconfirmed_name + "。确认请说：'是'")
+        self.speech("将命名为：" + self.unconfirmed_name + "。确认请说：'是'", filename=True)
         self.pending = "命名确认"
 
     def confirm_name(self):
@@ -180,7 +230,7 @@ class ChunJi:
             self.speech("当前目录下无txt文件")
         else:
             self.read_file_tts += "请说出文件对应的数字"
-            self.speech(self.read_file_tts)
+            self.speech(self.read_file_tts, filename=True)
             self.pending = "选文件"
             print(self.open_list)
 
@@ -194,9 +244,11 @@ class ChunJi:
             self.name = self.open_list[index]
             with open(self.name, "r") as f:
                 self.text = f.read()
-            self.speech('已打开文件:' + self.name)
+
             self.pending = ""
             self.have_file = True
+            self.cursor = len(self.text)
+            self.speech('已打开文件:' + self.name, filename=True)
 
     @staticmethod
     def audio_record(key):
@@ -240,9 +292,16 @@ class ChunJi:
         wf.close()
 
     @staticmethod
-    def speech(text, block=False):
-        if "." in text:
-            text = text.replace(".", "点")
+    def speech(text, block=False, filename=False, read_punc = False):
+        if filename:
+            if "." in text:
+                text = text.replace(".", "点")
+
+        if read_punc:
+            for i in text:
+                if i in PUNC.keys():
+
+
         print(text)
         tts(text)
         playsound("tts.wav", block=block)
